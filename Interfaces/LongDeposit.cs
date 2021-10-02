0cb9b82991678909f1abc276ec1af3cbcd83a904
Yevhenii Klimov <24evgeniy03@gmail.com>
2021-10-02 12:05:20 +0000

    Update LongDeposit.cs

--- Interfaces/LongDeposit.cs
+++ Interfaces/LongDeposit.cs
@@ -1,14 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Interfaces
 {
-    //TODO: Use class "LongDeposit" from previous task ("Aggregation").
+    public class LongDeposit : Deposit, IProlongable
+    {
+        public LongDeposit(decimal amount, int period) : base(amount, period) { }
 
-
-    //TODO: Implement interface "IProlongable" in class "LongDeposit".
-
-    //Interface's method should return true if deposit term was no more than 3 years.
-
+        public override decimal Income()
+        {
+            decimal result = 0;
+            if (Period > 0)
+            {
+                result = Amount;
+                for (int i = 0; i < Period; i++)
+                {
+                    if (i > 5)
+                        result += 0.15m * result;
+                }
+                if (result > Amount)
+                    return Math.Round(result - Amount, 2);
+                else return 0;
+            }
+            return result;
+        }
+        public bool CanToProlong()
+        {
+            return (Period <= 3 * 12);
+        }
+    }
 }
