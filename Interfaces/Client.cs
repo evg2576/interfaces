a39ff9a5f1f2d540977bda4fed027f3a1f51d160
Yevhenii Klimov <24evgeniy03@gmail.com>
2021-10-02 12:03:53 +0000

    Update Client.cs

--- Interfaces/Client.cs
+++ Interfaces/Client.cs
@@ -1,16 +1,103 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Interfaces
 {
-    //TODO: Use class "Client" from previous task ("Aggregation").
+    public class Client : IEnumerable<Deposit>
+    {
+        private Deposit[] deposits;
 
+        public Client()
+        {
+            deposits = new Deposit[10];
+        }
 
-    //TODO: Implement interface "IEnumerable<Deposit>" in class "Client".
+        public bool AddDeposit(Deposit deposit)
+        {
+            for (int i = 0; i < deposits.Length; i++)
+            {
+                if (deposits[i] == null)
+                {
+                    deposits[i] = deposit;
+                    return true;
+                }
+            }
+            return false;
+        }
 
-    //TODO: Create public method "SortDeposits" that sorts array of deposits by general sum of money in descending order.
+        public decimal TotalIncome()
+        {
+            decimal result = 0;
+            foreach (Deposit deposit in deposits)
+            {
+                if(deposit!= null)
+                    result += deposit.Income();
+            }
+            return result;
+        }
 
-    //TODO: Create public method "CountPossibleToProlongDeposit" that returns amount of current client's deposits that can be prolonged.
+        public decimal MaxIncome()
+        {
+            decimal max = decimal.MinValue;
+            for (int i = 0; i < deposits.Length; i++)
+            {
+                if (deposits[i] != null && deposits[i].Income() > max)
+                    max = deposits[i].Income();
+            }
+            return max;
+        }
 
+        public decimal GetIncomeByNumber(int number)
+        {
+            if (number <= deposits.Length && deposits[number - 1] != null)
+            {
+                return deposits[number - 1].Income();
+            }
+            else return 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Deposit deposit in deposits)
+            {
+                yield return deposit;
+            }
+        }
+
+        public void SortDeposits()
+        {
+            Array.Sort(deposits);
+            Array.Reverse(deposits);
+            
+        }
+
+        public int CountPossibleToProlongDeposit()
+        {
+            int count = 0;
+            IProlongable prolongable;
+            foreach (Deposit deposit in deposits)
+            {
+                if (deposit is IProlongable)
+                {
+                    prolongable = (IProlongable)deposit;
+                    if(prolongable.CanToProlong())
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator<Deposit> IEnumerable<Deposit>.GetEnumerator()
+        {
+            foreach (Deposit deposit in deposits)
+                yield return deposit;
+        }
+    }
 }
